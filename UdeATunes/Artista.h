b26a32b2b82ec2c04b6d971681d2b541271ef885
michell456 <michell.gaitan@udea.edu.co>
2025-10-21 22:22:31 -0500

    correccion en clase artista

--- UdeATunes/Artista.h
+++ UdeATunes/Artista.h
@@ -6,39 +6,32 @@ using namespace std;
 
 class Artista {
 private:
-    int id;                    // Código identificador (5 dígitos)
+    int id;
     int edad;
-    string paisOrigen;         // País de origen
-    int seguidores;           // Cantidad de seguidores en la plataforma
-    int posicionTendencias;   // Posición en lista de tendencias global
+    string paisOrigen;
+    int seguidores;
+    int posicionTendencias;
 
 public:
-    // Constructor
     Artista(int id, int edad, const string& pais, int seguidores, int posicion);
 
-    // Constructor de copia (default)
     Artista(const Artista& other) = default;
 
-    // Destructor (default)
     ~Artista() = default;
 
-    // Getters
     int getId() const;
     int getEdad() const;
     const string& getPaisOrigen() const;
     int getSeguidores() const;
     int getPosicionTendencias() const;
 
-    // Setters
     void setEdad(int nuevaEdad);
     void setSeguidores(int nuevosSeguidores);
     void setPosicionTendencias(int nuevaPosicion);
     void setPaisOrigen(const string& nuevoPais);
 
-    // Sobrecarga del operador de asignación (default)
     Artista& operator=(const Artista& other) = default;
 
-    // Sobrecarga del operador de igualdad
     bool operator==(const Artista& other) const;
 };
 
